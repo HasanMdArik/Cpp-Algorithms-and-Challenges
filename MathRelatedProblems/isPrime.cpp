ef10837cb5de4af761025719c72e9b10023574a0
Mohammad Arik <mohammadarik63@gmail.com>
2021-03-27 20:21:18 +0600

    is Prime Algorithm Added

--- MathRelatedProblems/isPrime.cpp
+++ MathRelatedProblems/isPrime.cpp
@@ -3,6 +3,23 @@ using namespace std;
 
 int main()
 {
-    cout << "It works!" << endl;
+    int number;
+    bool isPrime = true;
+    cout << "Please enter the number to check if that's a prime number or not" << endl
+         << "number > ";
+    cin >> number;
+
+    for (int i = 2; i <= (number / 2); i++)
+    {
+        if (number % i == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+
+    isPrime ? cout << "Prime Number" : cout << "Not Prime Number";
+    cout << endl;
+
     return 0;
 }

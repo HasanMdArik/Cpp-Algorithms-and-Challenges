484890d9ea6d20fa47c7e713b3df5a534304c1c6
MohammadArik <mohammadarik63@gmail.com>
2021-07-18 18:51:35 +0600

    hexaDecimal to Decimal added

--- MathRelatedProblems/hexaDecimalToDecimal.cpp
+++ MathRelatedProblems/hexaDecimalToDecimal.cpp
@@ -1,42 +1,25 @@
 #include <iostream>
-#include <cstdlib>
 using namespace std;
-
-int pow(int base, int power)
-{
-    if (power > 0)
-    {
-        int result = base;
-        for (int i = 1; i < power; i++)
-        {
-            result *= base;
-        }
-        return result;
-    }
-    else
-    {
-        return 1;
-    }
-}
 
 int hexToDecimal(string n)
 {
-    int result;
+    int result = 0;
+    int baseValue = 1;
+    int size = n.size();
 
-    for (int i = (n.length() - 1); i >= 0; i--)
+    for (int i = size - 1; i >= 0; --i)
     {
-        // char charcter = n[i];
-        // int number =
-        cout << n[i] << endl;
+        if (n[i] >= '0' && n[i] <= '9')
+        {
+            result += baseValue * (n[i] - '0');
+        }
+        else if (tolower(n[i]) >= 'a' && tolower(n[i]) <= 'f')
+        {
+            result += baseValue * (tolower(n[i]) - 'a' + 10);
+        }
+        baseValue *= 16;
     }
 
-    // for (int i = 0; n > 0; i++)
-    // {
-    //     int lastDigit = n % 10;
-    //     result += (lastDigit * pow(16, i));
-
-    //     n = (n / 10);
-    // }
     return result;
 }
 

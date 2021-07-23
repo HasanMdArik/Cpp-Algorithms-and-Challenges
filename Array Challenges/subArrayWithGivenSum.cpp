e8d02c2d17b243796bd7f70601e8151c8652a39a
MohammadArik <mohammadarik63@gmail.com>
2021-07-23 21:06:46 +0600

    sub array with given sum added

---
+++ Array Challenges/subArrayWithGivenSum.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char **argv)
+{
+    int n, sum;
+    cin >> n >> sum;
+
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int startIndex = -1, endIndex = -1, sumOfSubArray = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sumOfSubArray = 0;
+        for (int j = i; j < n; j++)
+        {
+            sumOfSubArray += a[j];
+
+            if (sumOfSubArray == sum)
+            {
+                endIndex = j;
+                startIndex = i;
+                break;
+            }
+        }
+        if (startIndex >= 0)
+        {
+            break;
+        }
+    }
+
+    cout << startIndex << " " << endIndex << endl;
+
+    return 0;
+}
